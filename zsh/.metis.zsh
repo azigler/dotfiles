85ba512089f1d97ef8c5596f3af875a9fd67540b
Andrew Zigler <andrewzigler@gmail.com>
2024-11-01 21:27:33 -0700

    🔧 (rc, .metis.zsh): Update environment variables and add PNPM_HOME path The VISUAL environment variable is now set to the value of the EDITOR variable, which is more efficient and avoids redundancy. The addition of the PNPM_HOME path to the PATH environment variable allows for the use of the pnpm package manager from any location in the terminal. This change enhances the development environment by providing more flexibility and efficiency in package management.

--- zsh/.metis.zsh
+++ zsh/.metis.zsh
@@ -1,7 +1,7 @@
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$HOME/.cargo/bin:/opt/homebrew/bin:/usr/local/share/dotnet/x64:$PATH"
 
-export VISUAL="vim"
 export EDITOR="vim"
+export VISUAL=$EDITOR
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -11,6 +11,12 @@ export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
+
+export PNPM_HOME="/Users/andrew/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
 
 __conda_setup="$('/Users/andrew/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
