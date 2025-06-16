40e97d36a2b8adc2a25390660055a934fdb173b8
Andrew Zigler <andrewzigler@gmail.com>
2025-06-16 14:46:25 -0700

    🔧 feat(zsh): add pyenv configuration and include local bin in PATH

--- zsh/.linearmac.zsh
+++ zsh/.linearmac.zsh
@@ -1,4 +1,4 @@
-export PATH="/opt/homebrew/bin:$PATH"
+export PATH="/opt/homebrew/bin:$HOME/.local/bin:$PATH"
 
 export EDITOR="vim"
 export VISUAL=$EDITOR
@@ -6,6 +6,11 @@ export VISUAL=$EDITOR
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
 export PNPM_HOME="/Users/zig/Library/pnpm"
 case ":$PATH:" in
